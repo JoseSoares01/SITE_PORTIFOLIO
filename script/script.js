d451a6bd98e828f8afd474e159b55ee4af6680d3
José Soares <janyelrodrigues@hotmail.com>
2024-11-08 23:12:50 +0000

    comitar e subir

---
+++ script/script.js
@@ -0,0 +1,10 @@
+// Alternância entre modo claro e escuro
+document.querySelector(".day-night input").addEventListener("change", () => {
+    document.querySelector("body").classList.add("toggle");
+    setTimeout(() => {
+      document.querySelector("body").classList.toggle("light");
+  
+      setTimeout(() => document.querySelector("body").classList.remove("toggle"), 10);
+    }, 5);
+  });
+  
