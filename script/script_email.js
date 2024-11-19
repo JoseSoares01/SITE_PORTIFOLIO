94563d0c3706ae6dc0b47210ef5ca7d94f01ced7
José Soares <janyelrodrigues@hotmail.com>
2024-11-19 15:59:05 +0000

    inclusão de nova função

---
+++ script/script_email.js
@@ -0,0 +1,21 @@
+// Função para enviar o formulário
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do formulário
+
+    // Coleta os dados do formulário
+    var userEmail = document.getElementById('user-email').value;
+    var userMessage = document.getElementById('user-message').value;
+
+    // Envia o e-mail usando EmailJS
+    emailjs.send('service_avnu3n5', 'template_gcw9mmgg', {
+            from_email: userEmail,
+            message: userMessage
+        })
+        .then(function(response) {
+            alert('Mensagem enviada com sucesso!');
+            console.log('Sucesso:', response);
+        }, function(error) {
+            alert('Falha ao enviar mensagem. Tente novamente!');
+            console.log('Erro:', error);
+        });
+});
