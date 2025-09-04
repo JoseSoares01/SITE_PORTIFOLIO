3170a3db1088c072df9f2ddba9dcffdb5bcdd797
José Soares <janyelrodrigues@hotmail.com>
2025-09-04 08:22:56 +0100

    alterações no modo de exibição

--- script/script-index.js
+++ script/script-index.js
@@ -1,45 +1,18 @@
-/* JavaScript */
-document.addEventListener('DOMContentLoaded', function() {
-    const buttonHamburguer = document.querySelector('.button-hamburguer');
-    const navbarCollapse = document.querySelector('#navbarResponsive');
+document.addEventListener('DOMContentLoaded', function () {
+  const navToggle = document.querySelector('.nav-toggle');
+  const navMenu = document.querySelector('.nav-menu');
 
-    buttonHamburguer.addEventListener('click', function() {
-        this.classList.toggle('active');
-        navbarCollapse.classList.toggle('show');
+  if (navToggle && navMenu) {
+    navToggle.addEventListener('click', function () {
+      navMenu.classList.toggle('active');
+      navToggle.classList.toggle('active'); // Adiciona animação ao ícone
     });
 
-    // Fechar menu ao clicar em um link
-    document.querySlectorAll('.nav-link').forEach(link => {
-        link.addEventListener('click', () => {
-            navbarCollapse.classList.remove('show');
-            buttonHamburguer.classList.remove('active');
-        });
+    document.querySelectorAll('.nav-item').forEach(link => {
+      link.addEventListener('click', () => {
+        navMenu.classList.remove('active');
+        navToggle.classList.remove('active');
+      });
     });
-});
-
-function toggleMenu() {
-    const hamburger = document.querySelector('.hamburger');
-    const mobileMenu = document.querySelector('.mobile-menu');
-    
-    hamburger.classList.toggle('active');
-    mobileMenu.classList.toggle('active');
   }
-
-  // Função que inicializa o mapa com Leaflet
-//function initMap() {
-    // Criação do mapa com as coordenadas desejadas
-    //var map = L.map('map').setView([40.730610, -73.935242], 13); // Coordenadas de exemplo (Nova York)
-  
-    // Adicionar camada do mapa usando OpenStreetMap
-    //L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      //attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    //}).addTo(map);
-  
-    // Adicionar marcador
-    //L.marker([40.730610, -73.935242]).addTo(map)
-      //.bindPopup("<b>José Soares</b><br>Portfólio.")
-      //.openPopup();
-//}
-
-// Chamar a função para inicializar o mapa
-//initMap();//
+});
